6a79592c2baa86688df8f5d93a094c12f1766f15
Susmit Ullas <susmitullas@gmail.com>
2023-01-27 01:28:59 +0530

    Update

---
+++ PrototypeWebsite-1 (In Progress)/script.js
@@ -0,0 +1,22 @@
+// typing animation script
+var typed = new Typed('.typing', 
+{
+    strings: ["Web Developer", "Freelancer", "Blogger", "Web Designer", "Content Writer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+// aside script
+
+    const navTogglerBtn = document.querySelector('.nav-toggler'),
+        aside = document.querySelector('.aside');
+        navTogglerBtn.addEventListener('click', () =>
+        {
+            asideSectionTogglerBtn();
+        })
+        function asideSectionTogglerBtn()
+        {
+            aside.classList.toggle('open');
+            navTogglerBtn.classList.toggle('open');
+        }
